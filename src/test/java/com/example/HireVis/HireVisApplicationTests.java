d3afec207cc74b3649a1ee4c10d70022a868db50
dev-tirth14 <dev.tirthp14@gmail.com>
2024-04-27 12:39:58 -0400

    Saving Spring initialized project

---
+++ src/test/java/com/example/HireVis/HireVisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HireVis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireVisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
